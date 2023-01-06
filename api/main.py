58d2c08433b173a871ea2c7c8f20922e909dd48e
Fischer0007 <nikitacebotov0@gmail.com>
2023-01-07 03:10:12 +0400

    first commit

---
+++ api/main.py
@@ -0,0 +1,37 @@
+
+from operator import itemgetter
+from flask import Flask, request, jsonify
+from flask_restful import Api
+from tinydb import TinyDB
+from models import *
+
+
+app = Flask(__name__)
+api = Api(app)
+app.config.from_pyfile('config.cfg')
+db = TinyDB('db/db.json')
+
+
+@app.route('/get_form/', methods=['POST'])
+def get_form():
+    template = db.table(name="_templates").all()[0]
+    list_template = list(template.values())
+    form = jsonify(request.values).get_json()
+    list_of_types = list_template + list(MyModel.parse_obj(form).dict().values())
+    if "FIELD_TYPE" not in list_of_types:
+        visited = set()
+        list_of_types[:] = [x for x in list_of_types if x not in visited and x != None and not visited.add(x)]
+        if list_template == list_of_types:
+            return list_of_types[0]
+    else:
+        response = dict(zip(list(MyModel.parse_obj(form).dict().keys()), MyModel.parse_obj(form).dict().values()))
+        return response
+
+
+@app.errorhandler(500)
+def handle_500(error):
+    return str(error), 500
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ api/models.py
@@ -0,0 +1,35 @@
+import re
+from typing import Optional, NewType
+from pydantic import BaseModel, validator
+
+
+Field_type = NewType("FIELD_TYPE", str)
+
+
+class MyModel(BaseModel):
+    atr_1: Optional[str]
+    atr_2: Optional[str]
+    atr_3: Optional[str]
+    atr_4: Optional[str]
+    atr_5: Optional[str]
+    atr_6: Optional[str]
+    atr_7: Optional[str]
+    atr_8: Optional[str]
+    atr_9: Optional[str]
+    atr_10: Optional[str]
+
+    @validator('atr_1', 'atr_2', 'atr_3', 'atr_4', 'atr_5', 'atr_6', 'atr_7', 'atr_8', 'atr_9', 'atr_10')
+    def validation(cls, v):
+        test = {"date": r'^\d{4}\-\d\d\-\d\d$',
+                "phone": r'^ |\+7|7\s*\d{3}\s*\d{3}\s*\d{2}\s*\d{2}$',
+                "email": r'^\S+@\w+.\w{2,4}$'}
+
+        for key, r in test.items():
+            if re.fullmatch(r, v):
+                return key
+            if re.fullmatch(r, v) == False:
+                return Field_type.__name__
+        if " " not in v:
+            return Field_type.__name__
+        else:
+            return "text"
